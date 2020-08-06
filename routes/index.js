e4706efb7a145b8525d1d1e25a47984ee7938a80
vishwa5854 <vishwanath5854@outlook.com>
2020-08-06 23:53:03 +0530

    added param support

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,8 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
-router.get("/videos", (request, response) => {
-    response.sendFile(path.resolve("D:\\Rec SMART INTERVIEWS\\session 1\\com\\Zoom 2020-04-18 16-42-38.mp4"));
-    // fs.createReadStream(path.resolve("D:\\Rec SMART INTERVIEWS\\session 25\\Zoom Meeting 2020-07-12 13-54-45-converted.mp4")).pipe(response);
+router.get("/videos/:video", (request, response) => {
+    response.sendFile(path.resolve("C:\\Users\\Administrator\\Downloads\\data\\" + request.params.video));
     console.log("Sending the video");
 });
 
